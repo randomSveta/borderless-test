f569ace692d93fcae76b62c2f9e75c4cf1dbb14f
@randomSveta <sveta_klimova@outlook.com>
2024-03-16 15:09:46 +0000

    feat: add link to full profile

--- src/components/CandidateCard/CandidateCard.tsx
+++ src/components/CandidateCard/CandidateCard.tsx
@@ -3,30 +3,36 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const CandidateCard: React.FC = ({ candidate }) => {
-  const { picture, name, location } = candidate;
+  const { picture, name, location, login } = candidate;
   console.log(candidate);
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="80"
-          image={picture.large}
-          alt={`Photo ${name.first} ${name.last}`}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {`${name.title} ${name.first} ${name.last}`}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {location.country}
-            <br />
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Link
+      to={`/candidate/${login.username}`}
+      style={{ textDecoration: "none" }}
+    >
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="80"
+            image={picture.large}
+            alt={`Photo ${name.first} ${name.last}`}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {`${name.title} ${name.first} ${name.last}`}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {location.country}
+              <br />
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 };
 
