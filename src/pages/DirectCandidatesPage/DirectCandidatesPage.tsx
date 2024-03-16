be677517844b4295813f0c858532572f39fc2dea
@randomSveta <sveta_klimova@outlook.com>
2024-03-16 12:21:20 +0000

    feat: show list of candidates

--- src/pages/DirectCandidatesPage/DirectCandidatesPage.tsx
+++ src/pages/DirectCandidatesPage/DirectCandidatesPage.tsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
+import { CircularProgress, Alert } from "@mui/material";
+import { ICandidate } from "./interfaces";
 
 const DirectCandidatesPage: React.FC = () => {
-  return (
+  const [candidateList, setCandidateList] = useState<ICandidate[]>([]);
+  const [isCandidateListLoaded, setIsCandidateListLoaded] =
+    useState<boolean>(false);
+  const [candidatesLoadingError, setCandidateLoadingError] =
+    useState<AxiosError | null>(null);
+
+  useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/?results=100")
+      .then((response) => {
+        setCandidateList(response.data.results);
+        setIsCandidateListLoaded(true);
+      })
+      .catch((error: AxiosError) => {
+        console.error("Error fetching data:", error);
+        setCandidateLoadingError(error);
+        setIsCandidateListLoaded(true);
+      });
+  }, []);
+
+  return isCandidateListLoaded ? (
     <div>
-      <h1>Hello, World!</h1>
+      {candidatesLoadingError ? (
+        <Alert severity="error">
+          {candidatesLoadingError.response ? (
+            <div>
+              <p>Status Code: {candidatesLoadingError.response.status}</p>
+              <p>
+                Data: {JSON.stringify(candidatesLoadingError.response.data)}
+              </p>
+              <p>
+                Headers:{" "}
+                {JSON.stringify(candidatesLoadingError.response.headers)}
+              </p>
+            </div>
+          ) : candidatesLoadingError.request ? (
+            <p>No response received from the server.</p>
+          ) : (
+            <p>Error: {candidatesLoadingError.message}</p>
+          )}
+        </Alert>
+      ) : (
+        <ul>
+          {candidateList.map((candidateInfo, index) => (
+            <li key={`candidate-${index}`}>{candidateInfo.name.first}</li>
+          ))}
+        </ul>
+      )}
     </div>
+  ) : (
+    <CircularProgress />
   );
 };
 
