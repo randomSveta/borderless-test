32e2b3c48d6979a923014a8ac5baeac2b200f642
@randomSveta <sveta_klimova@outlook.com>
2024-03-18 14:39:42 +0000

    fix: update import

--- src/components/SearchBar/interfaces.ts
+++ src/components/SearchBar/interfaces.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { ICandidate } from "../../pages/DirectCandidatesPage/interfaces";
+import { ICandidate } from "../../pages/CandidatePage/interfaces";
 export interface ISearchBarProps {
   setFilteredCandidates: React.Dispatch<React.SetStateAction<ICandidate[]>>;
   candidates: ICandidate[] | [];
