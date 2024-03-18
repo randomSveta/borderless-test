69337b68181a2a451d15b1e38ec26336ffbd2c0f
@randomSveta <sveta_klimova@outlook.com>
2024-03-18 14:23:38 +0000

    refactor: move data generation to CandidateList

--- src/components/CandidateList/CandidateList.tsx
+++ src/components/CandidateList/CandidateList.tsx
@@ -1,41 +1,84 @@
-import React, { useState } from "react";
-
-import { ICandidateListProps } from "./interfaces";
-
+import React, { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
+import { CircularProgress } from "@mui/material";
+import { useGlobalContext } from "../../GlobalContext"; // Import useGlobalContext
+import { ICandidate } from "../../pages/CandidatePage/interfaces";
+import ErrorAxios from "../../components/ErrorAxios";
 import CandidateCard from "../../components/CandidateCard";
 import { Pagination, Grid } from "@mui/material";
-
 import "./CandidateList.scss";
+import SearchBar from "../SearchBar";
 
-const CandidateList: React.FC<ICandidateListProps> = ({
-  filteredCandidates,
-}) => {
+const CandidateList: React.FC = () => {
+  const apiUrl: string | null = useGlobalContext(); // Use useGlobalContext to get apiUrl
+  const [candidateList, setCandidateList] = useState<ICandidate[]>([]);
+  const [filteredCandidates, setFilteredCandidates] = useState<ICandidate[]>(
+    [],
+  );
+  const [isCandidateListLoaded, setIsCandidateListLoaded] =
+    useState<boolean>(false);
+  const [candidatesLoadingError, setCandidatesLoadingError] =
+    useState<AxiosError | null>(null);
   const [page, setPage] = useState(1);
   const itemsPerPage = 8;
 
-  const startIndex = (page - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
+  useEffect(() => {
+    if (apiUrl) {
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          setCandidateList(response.data.results);
+          setIsCandidateListLoaded(true);
+        })
+        .catch((error: AxiosError) => {
+          console.error("Error fetching data:", error);
+          setCandidatesLoadingError(error);
+          setIsCandidateListLoaded(true);
+        });
+    }
+  }, [apiUrl]);
+
+  useEffect(() => {
+    setFilteredCandidates(candidateList);
+  }, [candidateList]);
 
   const handlePageChange = (_: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
 
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  if (!isCandidateListLoaded) {
+    return <CircularProgress data-testid="loading-spinner" />;
+  }
+
+  if (candidatesLoadingError) {
+    return <ErrorAxios error={candidatesLoadingError} />;
+  }
+
   return (
-    <section className="CandidateList">
+    <section className="CandidateList" data-testid="candidate-list">
+      <div className="direct-candidates__filters">
+        <SearchBar
+          setFilteredCandidates={setFilteredCandidates}
+          candidates={candidateList}
+        />
+      </div>
       <Grid
         container
         spacing={{ xs: 1, md: 2 }}
         columns={{ xs: 1, sm: 8, md: 16 }}
       >
         {filteredCandidates
+          .slice(startIndex, endIndex)
           .map((candidateInfo, index) => (
             <Grid item xs={2} sm={4} md={4} key={index}>
               <CandidateCard candidate={candidateInfo} />
             </Grid>
-          ))
-          .slice(startIndex, endIndex)}
+          ))}
       </Grid>
-      {filteredCandidates.length > 8 ? (
+      {filteredCandidates.length > itemsPerPage && (
         <Pagination
           className="Pagination"
           count={Math.ceil(filteredCandidates.length / itemsPerPage)}
@@ -44,7 +87,7 @@ const CandidateList: React.FC<ICandidateListProps> = ({
           variant="outlined"
           shape="rounded"
         />
-      ) : null}
+      )}
     </section>
   );
 };
--- src/pages/DirectCandidatesPage/DirectCandidatesPage.tsx
+++ src/pages/DirectCandidatesPage/DirectCandidatesPage.tsx
@@ -1,58 +1,9 @@
-import React, { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
-
-import { useGlobalContext } from "../../GlobalContext";
-
-import { ICandidate } from "./interfaces";
-
-import { CircularProgress, Paper, Typography } from "@mui/material";
-import ErrorAxios from "../../components/ErrorAxios";
-import SearchBar from "../../components/SearchBar";
+import React from "react";
+import { Paper, Typography } from "@mui/material";
 import CandidateList from "../../components/CandidateList";
-
 import "./DirectCandidatesPage.scss";
 
 const DirectCandidatesPage: React.FC = () => {
-  const [candidateList, setCandidateList] = useState<ICandidate[]>([]);
-  const [filteredCandidates, setFilteredCandidates] = useState<
-    ICandidate[] | []
-  >([]);
-  const [isCandidateListLoaded, setIsCandidateListLoaded] =
-    useState<boolean>(false);
-  const [candidatesLoadingError, setCandidatesLoadingError] =
-    useState<AxiosError | null>(null);
-
-  const apiUrl: string | null = useGlobalContext();
-
-  useEffect(() => {
-    if (apiUrl) {
-      axios
-        .get(apiUrl)
-        .then((response) => {
-          setCandidateList(response.data.results);
-        })
-        .catch((error: AxiosError) => {
-          console.error("Error fetching data:");
-          setCandidatesLoadingError(error);
-        })
-        .finally(() => {
-          setIsCandidateListLoaded(true);
-        });
-    }
-  }, []);
-
-  useEffect(() => {
-    setFilteredCandidates(candidateList);
-  }, [candidateList]);
-
-  if (candidatesLoadingError) {
-    return <ErrorAxios error={candidatesLoadingError} />;
-  }
-
-  if (!isCandidateListLoaded) {
-    return <CircularProgress data-testid="loading-spinner" />;
-  }
-
   return (
     <div className="DirectCandidatesPage" data-testid="direct-candidates-page">
       <Paper elevation={1} className="direct-candidates__info">
@@ -61,13 +12,7 @@ const DirectCandidatesPage: React.FC = () => {
           These candidates have applied to you directly
         </Typography>
       </Paper>
-      <div className="direct-candidates__filters">
-        <SearchBar
-          setFilteredCandidates={setFilteredCandidates}
-          candidates={candidateList}
-        />
-      </div>
-      <CandidateList filteredCandidates={filteredCandidates} />
+      <CandidateList />
     </div>
   );
 };
